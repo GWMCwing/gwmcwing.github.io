bec6823109423d0e75b4b5d7836b1d8b8306580e
GW_MC <ping.chan78@gmail.com>
2022-01-18 13:22:02 +0800

    finsih navbar layout (desktop/mobile)

--- res/javascripts/navbar.js
+++ res/javascripts/navbar.js
@@ -1,9 +1,16 @@
-const navbarOptionGear = document.getElementsByClassName('navbar-mobile-option-button-icon-wheel')[0];
-const navbarOptionCross = document.getElementsByClassName('navbar-mobile-option-button-icon-cross')[0];
+const navbarContiner = document.getElementById('navbar');
+const navbarOptionGear = document.getElementsByClassName(
+	'navbar-mobile-option-button-icon-wheel'
+)[0];
+const navbarOptionCross = document.getElementsByClassName(
+	'navbar-mobile-option-button-icon-cross'
+)[0];
 
 function toggleMobileOption() {
 	navbarOptionGear.classList.toggle('show');
 	navbarOptionCross.classList.toggle('show');
 	navbarOptionGear.classList.toggle('hide');
 	navbarOptionCross.classList.toggle('hide');
+	navbarContiner.classList.toggle('collapsed');
+	navbarContiner.classList.toggle('expanded');
 }
