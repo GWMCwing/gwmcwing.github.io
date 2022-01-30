86bb0855019419af1f1e19c28ae36892c7574ab2
GW_MC <ping.chan78@gmail.com>
2022-01-30 22:29:53 +0800

    basic ui design pathFinding

--- res/javascripts/visualization/pathFinding/pathFindingMain.js
+++ res/javascripts/visualization/pathFinding/pathFindingMain.js
@@ -1,4 +1,5 @@
 var boxList2d = [];
+var boxClassLoaded = false;
 //
 class boxClass {
 	constructor(i, j, element) {
@@ -12,42 +13,22 @@ window.onload = function () {
 	setOptionsRange();
 	setOptionsListener();
 	insertBox();
+	boxClassLoaded = true;
+	updateGridSize();
+};
+window.onresize = function () {
 	updateGridSize();
 };
 //
 function setOptionsRange() {
-	[
-		pathFindingSpeed_range.min,
-		pathFindingSpeed_range.max,
-		pathFindingSpeed_range.value,
-	] = speedRange;
-	[
-		pathFindingSpeed_number.min,
-		pathFindingSpeed_number.max,
-		pathFindingSpeed_number.value,
-	] = speedRange;
+	[pathFindingSpeed_range.min, pathFindingSpeed_range.max, pathFindingSpeed_range.value] = speedRange;
+	[pathFindingSpeed_number.min, pathFindingSpeed_number.max, pathFindingSpeed_number.value] = speedRange;
 	//
-	[
-		pathFindingWidth_range.min,
-		pathFindingWidth_range.max,
-		pathFindingWidth_range.value,
-	] = widthRange;
-	[
-		pathFindingWidth_number.min,
-		pathFindingWidth_number.max,
-		pathFindingWidth_number.value,
-	] = widthRange;
+	[pathFindingWidth_range.min, pathFindingWidth_range.max, pathFindingWidth_range.value] = widthRange;
+	[pathFindingWidth_number.min, pathFindingWidth_number.max, pathFindingWidth_number.value] = widthRange;
 	//
-	[
-		pathFindingHeight_range.min,
-		pathFindingHeight_range.max,
-		pathFindingHeight_range.value,
-	] = heightRange;
-	[
-		pathFindingHeight_number.min,
-		pathFindingHeight_number.max,
-		pathFindingHeight_number.value,
-	] = heightRange;
+	[pathFindingHeight_range.min, pathFindingHeight_range.max, pathFindingHeight_range.value] = heightRange;
+	[pathFindingHeight_number.min, pathFindingHeight_number.max, pathFindingHeight_number.value] = heightRange;
 }
 function setOptionsListener() {
 	pathFindingSpeed_range.oninput = function () {
@@ -104,28 +85,30 @@ function setOptionsListener() {
 	};
 }
 function updateGridSize() {
+	if (!boxClassLoaded) return;
 	const widthNum = parseInt(pathFindingWidth_number.value);
 	const heightNum = parseInt(pathFindingHeight_number.value);
-
 	const boxSize = Math.min(
-		window.innerWidth / widthNum,
-		window.innerHeight / heightNum
+		window.innerWidth / (widthNum + 6),
+		((window.innerHeight / (heightNum + 6)) * contentVhPercentage) / 100
 	);
-	pathFindingBox_display.style.gridTemplateColumns = `repeat(${widthNum}, ${boxSize}px)`;
-	pathFindingBox_display.style.gridTemplateRows = `repeat(${heightNum}, ${boxSize}px)`;
+	const gapSize = boxSize * 0.08;
+	pathFindingBox_display.style.gridTemplateColumns = `repeat(${widthNum}, ${boxSize - gridGapSize_Px}px)`;
+	pathFindingBox_display.style.gridTemplateRows = `repeat(${heightNum}, ${boxSize - gridGapSize_Px}px)`;
+	pathFindingBox_display.style.gap = `${gapSize}px`;
+	pathFindingBox_display.style.width = `${widthNum * boxSize + (widthNum - 2) * gridGapSize_Px}px`;
+	// pathFindingBox_display.style.height = `${heightNum * boxSize + (heightNum - 2) * gridGapSize_Px}px`;
 	//
 	const iLen = boxList2d.length;
 	const jLen = boxList2d[0].length;
-	for (let i = 0; i < heightNum; i++) {
-		for (let j = 0; j < widthNum; j++) {
+	for (let i = 0; i < iLen; i++) {
+		for (let j = 0; j < jLen; j++) {
 			const box = boxList2d[i][j];
-			box.element.display = 'block';
-		}
-	}
-	for (let i = heightNum; i < iLen; i++) {
-		for (let j = widthNum; j < jLen; j++) {
-			const box = boxList2d[i][j];
-			box.element.display = 'none';
+			if (i < heightNum && j < widthNum) {
+				box.element.style.display = 'block';
+			} else {
+				box.element.style.display = 'none';
+			}
 		}
 	}
 }
